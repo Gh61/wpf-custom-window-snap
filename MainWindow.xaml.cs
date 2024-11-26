3f6967158cde2cba63237df2649943130ae8aa03
Gh61 <T.Zenkner@email.cz>
2024-11-26 12:20:40 +0100

    Created sln + Window

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace WpfWindowTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+
+            BarTitle.MouseLeftButtonDown += (s, a) => DragMove();
+        }
+
+        private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnMaximize_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState == WindowState.Normal
+                ? WindowState.Maximized
+                : WindowState.Normal;
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
